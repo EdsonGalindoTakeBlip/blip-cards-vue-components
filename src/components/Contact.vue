bb3150cdff4145730facf06a1bd7618d29d410a7
Edson Galindo <edson.galindo@take.net>
2023-05-02 18:22:31 -0300

    Added the Contact Vue component

---
+++ src/components/Contact.vue
@@ -0,0 +1,33 @@
+<template>
+  
+</template>
+
+<script lang="js">
+
+  export default  {
+    name: 'contact',
+    props: [],
+    mounted () {
+
+    },
+    data () {
+      return {
+
+      }
+    },
+    methods: {
+
+    },
+    computed: {
+
+    }
+}
+
+
+</script>
+
+<style scoped lang="scss">
+  .contact {
+
+  }
+</style>
